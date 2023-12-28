01feb3168e4e5e0011163500cfeb2f306f856825
Mathias Jensen <mathias@Mathiass-MacBook-Pro.local>
2023-12-28 14:28:01 +0100

    kafka test push

--- src/test/java/com/example/orderservice/kafka/KafkaTest.java
+++ src/test/java/com/example/orderservice/kafka/KafkaTest.java
@@ -1,8 +1,6 @@
 package com.example.orderservice.kafka;
 
 
-import com.example.orderservice.service.KafkaService;
-import com.example.orderservice.service.KafkaServiceImpl;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
